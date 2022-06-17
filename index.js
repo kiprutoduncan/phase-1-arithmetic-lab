16313f82dfc62dc19c4c64b93687825222fb384b
Kipruto Duncan <kiprutoduncan36@gmail.com>
2022-06-17 16:50:41 +0300

    Done.

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const random = Math.floor( Math.random() * 1000 ) + 1;
 const num3 = 16;
 const num4 = 6;
 const mod = num3 % num4;
-const max = Math.max( 5, 10, 15, 20 );
+const max = Math.max( 6, 12, 18, 20 );
